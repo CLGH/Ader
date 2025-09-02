588f31c8dfcebc385766213fe87799d735c0f257
CLGH <cl.guth@free.fr>
2025-09-02 11:15:31 +0200

    Make face view.

--- adrLibPart.py
+++ adrLibPart.py
@@ -174,7 +174,7 @@ def MakeFrame(frameHeight, frameWidth, offset, x=0, fixedFrame= True, nbPoints=8
   return sk
 
 
-def MakeTopSketch(aircraftLength, aircraftWidth, fixedFrame= True, nbPoints=8, name='skTopView', plane='XY', body=None):
+def MakeTopSketch(aircraftLength, aircraftWidth, xRelMax= 0.33, fixedFrame= True, name='skTopView', plane='XY', body=None):
   doc=App.ActiveDocument
   if doc == None:
     raise Exception("Pas de document actif") 
@@ -187,8 +187,7 @@ def MakeTopSketch(aircraftLength, aircraftWidth, fixedFrame= True, nbPoints=8, n
 
   #l_2=aircraftLength/2
   w_2=aircraftWidth/2
-  xRelMax= 0.33
-
+  
   # Points 
   vects=[]
   #   Points from tail (elements 0..NbPointsTail-1)
@@ -276,6 +275,102 @@ def MakeTopSketch(aircraftLength, aircraftWidth, fixedFrame= True, nbPoints=8, n
   
   return sk
   
+def MakeFaceSketch(aircraftLength, aircraftHeight, xRelMax= 0.33, fixedFrame= True, name='skTopView', plane='XY', body=None):
+  doc=App.ActiveDocument
+  if doc == None:
+    raise Exception("Pas de document actif") 
+  if body==None:
+    body=doc.getObject('Body')
+    if body == None:
+      raise Exception("Pas de corps actif") 
+	
+  sk = NewSketch(name, plane, body)
+
+  #l_2=aircraftLength/2
+  h_2=aircraftHeight/2
+  
+  # Points 
+  vects=[]
+  #   Points from tail (elements 0..NbPointsTail-1)
+  NbPointsTail=4
+  step=(1-xRelMax)*aircraftLength/(NbPointsTail-1)
+  for i in range(0, NbPointsTail):
+    x=aircraftLength -i*step
+    y=h_2 * (1+i/3)
+    vects.append(App.Vector(x,y,0))
+  xMax=x
+  #   Points to front (elements NbPointsTai1..NbPointsTail+NbPointsFront-1)
+  NbPointsFront=5
+  step=xRelMax*aircraftLength/NbPointsFront
+  for i in range(1, NbPointsFront+1):
+    x=xMax-i*step
+    y= h_2 * (1 + sqrt(2*x*xMax - x*x) / xMax)
+    vects.append(App.Vector(x,y,0))
+  #   symetric points
+  for vect in vects[-2::-1]:
+    vects.append(App.Vector(vect.x,aircraftHeight-vect.y,0))
+
+  # test
+  #for vect in vects:
+  #  sk.addGeometry(Part.Point(vect),False)
+
+  MakeSpline(vects, perodic=False, sk=sk)
+
+  # Constraints on spline
+  pointTailFirstIx=0                                  # first point : start at tail
+  pointTailLastIx=2*(NbPointsTail+NbPointsFront-1)    # last point : end at tail
+  pointFrontIx=NbPointsTail+NbPointsFront-1           # front point element index
+  splineIx=pointTailLastIx+1
+  pointTopIx=NbPointsTail-1                           # top point at width/2 index
+  pointBottompIx=pointTopIx + 2*NbPointsFront         # bottom point at -width/2 index
+  constraintList = []
+  #   front point on origin
+  constraintList.append(Sketcher.Constraint('Coincident', pointFrontIx,1, -1,1))  
+  #   first and last points coïncident on axis
+  #constraintList.append(Sketcher.Constraint('Coincident',   pointTailFirstIx,1, pointTailLastIx,1))  # redondant ?
+  constraintList.append(Sketcher.Constraint('PointOnObject', splineIx,1,      -1))  
+
+  #sk.addConstraint(constraintList)
+  del constraintList
+
+  # create limit frame (elements sk.GeometryCount ..+3, top horizontal line first, clock wise)
+  topFrameIx=sk.GeometryCount 
+  rearFrameIx=topFrameIx+1
+  bottomFrameIx=topFrameIx+2   
+  frontFrameIx=topFrameIx+3
+  constrGeoList = []
+  constrGeoList.append(Part.LineSegment(App.Vector(0,              aircraftHeight, 0), App.Vector(aircraftLength, aircraftHeight, 0)))
+  constrGeoList.append(Part.LineSegment(App.Vector(aircraftLength, aircraftHeight, 0), App.Vector(aircraftLength, 0,              0)))
+  constrGeoList.append(Part.LineSegment(App.Vector(aircraftLength, 0,              0), App.Vector(0,              0,              0)))
+  constrGeoList.append(Part.LineSegment(App.Vector(0,              0,              0), App.Vector(0,              aircraftHeight, 0)))
+  sk.addGeometry(constrGeoList,True)
+  del constrGeoList
+  
+  constraintList = []
+  constraintList.append(Sketcher.Constraint('Coincident', topFrameIx,2,    rearFrameIx,1))
+  constraintList.append(Sketcher.Constraint('Coincident', rearFrameIx,2,   bottomFrameIx,1))
+  constraintList.append(Sketcher.Constraint('Coincident', bottomFrameIx,2, frontFrameIx,1))
+  constraintList.append(Sketcher.Constraint('Coincident', frontFrameIx,2,  topFrameIx,1))
+  constraintList.append(Sketcher.Constraint('Horizontal', topFrameIx))
+  constraintList.append(Sketcher.Constraint('Vertical',   rearFrameIx))
+  constraintList.append(Sketcher.Constraint('Horizontal', bottomFrameIx))
+  constraintList.append(Sketcher.Constraint('Vertical',   frontFrameIx))
+
+  if fixedFrame:
+    constraintList.append(Sketcher.Constraint('DistanceY', frontFrameIx,1, frontFrameIx,2, aircraftHeight))
+    constraintList.append(Sketcher.Constraint('DistanceX', frontFrameIx,2, topFrameIx,2,   aircraftLength))
+  
+  # sketch within frame
+  constraintList.append(Sketcher.Constraint('PointOnObject', pointFrontIx,1,     frontFrameIx))
+  constraintList.append(Sketcher.Constraint('PointOnObject', pointTopIx,1,       topFrameIx))
+  constraintList.append(Sketcher.Constraint('PointOnObject', pointBottompIx,1,   bottomFrameIx))
+  constraintList.append(Sketcher.Constraint('PointOnObject', splineIx,1,         rearFrameIx))
+
+  #sk.addConstraint(constraintList)
+  del constraintList
+  
+  return sk
+
 
 def MakePad(sketch, length, name= 'Pad', reversed=0, midplane=0, offset=0):
   body=Gui.ActiveDocument.ActiveView.getActiveObject('pdbody')
